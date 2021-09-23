0b5b937ab7a528f6727e87fa637cf023f6ef2a6e
Kojiro ONO <ono.kojiro@gmail.com>
2021-09-23 22:33:56 +0900

    add openbsd script

---
+++ kvm/create_openbsd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+name=openbsd
+disk=`pwd`/${name}.qcow2
+qemu-img create -f qcow2 $disk 16G
+
+#iso=`pwd`/FreeBSD-13.0-RELEASE-amd64-disc1.iso
+iso=~/Downloads/install69.iso
+
+virt-install \
+  --name ${name} \
+  --ram 4096 \
+  --disk=$disk,bus=virtio \
+  --vcpus 2 \
+  --os-type bsd \
+  --os-variant openbsd6.3 \
+  --network network=default --noautoconsole \
+  --console pty,target_type=serial \
+  --cdrom=$iso \
+  --graphics vnc,password=vnc,listen=0.0.0.0,keymap=ja \
+  --serial pty
+
+#  --extra-args 'console=ttyS0,115200n8 serial'
+
