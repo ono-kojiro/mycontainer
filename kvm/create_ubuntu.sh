789d2c917d42cc8a5f2397db1f0086f04dd6a284
Kojiro ONO <ono.kojiro@gmail.com>
2021-12-04 19:57:51 +0900

    update how to enable console on ubuntu 18.04

--- kvm/create_ubuntu.sh
+++ kvm/create_ubuntu.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-disk=`pwd`/ubuntu1804.qcow2
+disk=`pwd`/ubuntu.qcow2
 qemu-img create -f qcow2 $disk 16G
 
+iso=~/Downloads/ubuntu-18.04.6-live-server-amd64.iso
+
 virt-install \
---name ubuntu1804 \
+--name ubuntu \
 --ram 4096 \
 --disk=$disk,bus=virtio \
---vcpus 2 \
+--vcpus 4 \
 --os-type linux \
 --os-variant ubuntu18.04 \
 --graphics none \
@@ -15,5 +17,17 @@ virt-install \
 --location 'http://jp.archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/' \
 --extra-args 'console=ttyS0,115200n8 serial'
 
+#--location 'http://jp.archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/' \
 #--graphics vnc,password=vnc,listen=0.0.0.0,keymap=ja 
+
+
+# インストール終了直後、一旦ホスト側に戻り、ゲストをシャットダウン
+# root@dlp:~# virsh shutdown ubuntu1804
+# Domain template is being shutdown
+# ゲスト領域をマウントしてサービスを有効にする
+# root@dlp:~# guestmount -d ubuntu1804 -i /mnt
+# root@dlp:~# ln -s /mnt/lib/systemd/system/getty@.service /mnt/etc/systemd/system/getty.target.wants/getty@ttyS0.service
+# root@dlp:~# umount /mnt
+# 再度起動し、コンソールに接続できればインストール完了
+# root@dlp:~# virsh start ubuntu1804 --console
 
