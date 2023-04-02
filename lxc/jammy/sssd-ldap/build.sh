92ea04d3c6767c43c29d981cbaac0594f8a2c676
Kojiro ONO <ono.kojiro@gmail.com>
2023-04-02 22:52:25 +0900

    add playbook for sssd-ldap

---
+++ lxc/jammy/sssd-ldap/build.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+help()
+{
+  usage
+}
+
+usage()
+{
+  echo "usage : $0 [options] target1 target2 ..."
+cat - << EOS
+  target:
+    all
+EOS
+}
+
+all()
+{
+  ansible-playbook -i hosts site.yml
+}
+
+default()
+{
+  tag=$1
+  ansible-playbook -i hosts --tag $tag site.yml
+}
+
+debug()
+{
+  ansible-playbook -i hosts debug.yml
+}
+
+args=""
+while [ $# -ne 0 ]; do
+  case $1 in
+    -h )
+      usage
+      exit 1
+      ;;
+    -v )
+      verbose=1
+      ;;
+    * )
+      args="$args $1"
+      ;;
+  esac
+  
+  shift
+done
+
+if [ -z "$args" ]; then
+  help
+  exit 1
+fi
+
+for arg in $args; do
+  LANG=C type $arg | grep 'function' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    $arg
+  else
+    default $arg
+  fi
+done
+
