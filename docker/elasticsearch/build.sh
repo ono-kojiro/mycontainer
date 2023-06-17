d8973228710afbf1fa825875de76135d1390d07b
Kojiro ONO <ono.kojiro@gmail.com>
2023-06-17 15:24:32 +0900

    enable ssl for elasticsearch

--- docker/elasticsearch/build.sh
+++ docker/elasticsearch/build.sh
@@ -18,12 +18,9 @@ usage : $0 [options] target1 target2 ...
     create
     start
 
-    get
-    patch
-    put
-
-    stop
-
+    chpasswd
+    
+    restart
     down
     destroy
 EOS
@@ -36,77 +33,35 @@ all()
 
 prepare()
 {
-  sudo mkdir -p /var/lib/elasticsearch
-  sudo mkdir -p /var/lib/elasticsearch/certs
-  sudo mkdir -p /var/lib/elasticsearch/data
-  sudo mkdir -p /var/lib/elasticsearch/etc
-
-  sudo cp -f elasticsearch.crt /var/lib/elasticsearch/certs/
-  sudo cp -f elasticsearch.key /var/lib/elasticsearch/certs/
-  sudo cp -f mylocalca.pem     /var/lib/elasticsearch/certs/
+  cat - << 'EOF' | sudo bash -s
+    rm -rf /var/lib/elasticsearch
+    mkdir -p /var/lib/elasticsearch
+    mkdir -p /var/lib/elasticsearch/certs
+    mkdir -p /var/lib/elasticsearch/data
+    mkdir -p /var/lib/elasticsearch/etc
   
-  sudo chown -R 1000:1000 /var/lib/elasticsearch
+    cp -f elasticsearch.yml /var/lib/elasticsearch/
+    cp -f elasticsearch.crt /var/lib/elasticsearch/certs/
+    cp -f elasticsearch.key /var/lib/elasticsearch/certs/
+    cp -f mylocalca.pem     /var/lib/elasticsearch/certs/
+    
+    chown -R 1000:1000 /var/lib/elasticsearch
   
-  sudo mkdir -p /var/lib/kibana
-  sudo mkdir -p /var/lib/kibana/config/certs
-  sudo cp    -f mylocalca.pem     /var/lib/kibana/config/certs/
-  sudo cp    -f kibana.yml        /var/lib/kibana/config/
-  sudo chown -R 1000:1000 /var/lib/kibana
-  
+    mkdir -p /var/lib/kibana
+    mkdir -p /var/lib/kibana/config/certs
+    mkdir -p /var/lib/kibana/data
+    cp    -f mylocalca.pem     /var/lib/kibana/config/certs/
+    cp    -f kibana.crt        /var/lib/kibana/config/certs/
+    cp    -f kibana.key        /var/lib/kibana/config/certs/
+    cp    -f kibana.yml        /var/lib/kibana/config/
+    chown -R 1000:1000 /var/lib/kibana
+EOF
 }
 
 restart()
 {
   stop
   start
-}
-
-pull()
-{
-  docker cp kibana:/usr/share/kibana/config/kibana.yml .
-}
-
-enable_https()
-{
-  echo "enable https"
-  config="/usr/share/elasticsearch/config"
-  user="elasticsearch"
-  name="elasticsearch"
-
-  docker exec --user root $name mkdir -p $config/certs/
-  docker cp elasticsearch.yml $name:$config/
-  docker cp mylocalca.pem $name:$config/certs/
-  docker cp elasticsearch.key $name:$config/certs/
-  docker cp elasticsearch.crt $name:$config/certs/
-
-  docker exec --user root $name chown $name:root $config/elasticsearch.yml
-  docker exec --user root $name chown -R $name:root $config/certs/
-  
-}
-
-push()
-{
-  echo "change elasticsearch config"
-  config="/usr/share/elasticsearch/config"
-  user="elasticsearch"
-
-  docker cp elasticsearch.yml elasticsearch:$config/
-  docker exec --user root elasticsearch \
-    chown root.root $config/elasticsearch.yml
-  docker exec --user elasticsearch elasticsearch mkdir -p $config/certs/
-  docker cp mylocalca.pem elasticsearch:$config/certs/
-  docker cp elasticsearch.key elasticsearch:$config/certs/
-  docker cp elasticsearch.crt elasticsearch:$config/certs/
-  docker exec --user root elasticsearch chown -R elasticsearch:elasticsearch $config/certs/
-}
-
-push_kibana()
-{
-  echo "change kibana config"
-  docker cp kibana.yml kibana:/usr/share/kibana/config/kibana.yml
-  docker exec --user kibana kibana mkdir -p /usr/share/kibana/config/certs/
-  docker exec --user root kibana chown -R 1000:1000 /usr/share/kibana/config/certs/
-  docker cp mylocalca.pem     kibana:/usr/share/kibana/config/certs/
 }
 
 test()
@@ -136,16 +91,57 @@ test_https()
   #w3m -dump http://192.168.0.98:5601/
 }
 
-
-passwd()
+chpasswd()
 {
    echo "Setting kibana_system password";
-   curl -s \
+   curl \
      -X POST --cacert mylocalca.pem \
      -u "elastic:elastic" \
      -H "Content-Type: application/json" \
      https://192.168.0.98:9200/_security/user/kibana_system/_password \
      -d "{ \"password\":\"elastic\" }"
+}
+
+test_access()
+{
+   curl \
+     -X GET --cacert mylocalca.pem \
+     -u "kibana_system:elastic" \
+     -H "Content-Type: application/json" \
+     https://192.168.0.98:9200/ 
+}
+
+create_user()
+{
+  es_host="192.168.0.98:9200"
+
+  username=$USER
+  fullname=`git config --get user.name`
+  email=`git config --get user.mail`
+
+  stty -echo
+  printf "Password: "
+  read PASSWORD
+  stty echo
+  printf "\n"
+
+  password=$PASSWORD
+
+  curl -k --netrc-file ./.netrc \
+    -H 'Content-Type: application/json' \
+    -XPOST "https://$es_host/_security/user/$username?pretty" --data @- << EOS
+{
+  "password" : "$password",
+  "enabled" : true,
+  "roles" : [ "superuser", "kibana_admin" ],
+  "full_name" : "$fullname",
+  "email" : "$email",
+  "metadata" : {
+    "intelligence" : 7
+  }
+}
+EOS
+
 }
 
 debug()
@@ -177,7 +173,6 @@ attach()
 {
   docker exec -it --user root elasticsearch /bin/bash
 }
-
 
 stop()
 {
