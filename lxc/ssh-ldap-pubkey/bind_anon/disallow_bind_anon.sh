76e44318decc11c1b3a0d58ceb0fdb689558c84b
Kojiro ONO <ono.kojiro@gmail.com>
2023-07-02 17:10:29 +0900

    add disallow_bind_anon.sh

---
+++ lxc/ssh-ldap-pubkey/bind_anon/disallow_bind_anon.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+suffix=`cat /etc/ldap/ldap.conf | grep -e '^BASE' | awk '{ print $2 }'`
+masterdn="cn=Manager,${suffix}"
+
+stty -echo
+printf "Password for masterdn: "
+read PASSWORD
+stty echo
+printf "\n"
+
+cat - << EOF | ldapmodify -D $masterdn -w $PASSWORD
+dn: cn=config
+changetype: modify
+replace: olcDisallows
+olcDisallows: bind_anon
+EOF
+
+if [ $? -eq 0 ]; then
+  echo "ldapmodify passed"
+else
+  echo "ldapmodify failed"
+fi
+
+cat - << EOF | ldapmodify -D $masterdn -w $PASSWORD
+dn: cn=config
+changetype: modify
+replace: olcRequires
+olcRequires: authc
+EOF
+
+if [ $? -eq 0 ]; then
+  echo "ldapmodify passed"
+else
+  echo "ldapmodify failed"
+fi
+
+
+cat - << EOF | ldapmodify -D $masterdn -w $PASSWORD
+dn: olcDatabase={-1}frontend,cn=config
+changetype: modify
+replace: olcRequires
+olcRequires: authc
+EOF
+
+if [ $? -eq 0 ]; then
+  echo "ldapmodify passed"
+else
+  echo "ldapmodify failed"
+fi
+
+
