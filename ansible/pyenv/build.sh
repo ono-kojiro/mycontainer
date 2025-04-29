708887e0cff14fa410a5cf650e327ba48e354c2b
Kojiro ONO <ono.kojiro@gmail.com>
2025-04-29 16:21:16 +0900

    add playbook for pyenv

---
+++ ansible/pyenv/build.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+help()
+{
+  usage
+}
+
+usage()
+{
+  cat << EOS
+usage : $0 [OPTION...] [TARGET...]
+
+target:
+  deploy
+EOS
+
+}
+
+all()
+{
+  deploy
+}
+
+prepare()
+{
+  :
+}
+
+hosts()
+{
+  ansible-inventory -i template.yml --list --yaml > hosts.yml
+}
+
+deploy()
+{
+  ansible-playbook -K -i hosts.yml site.yml
+}
+
+default()
+{
+  tag=$1
+  ansible-playbook -K -i hosts.yml -t $tag site.yml
+}
+
+hosts
+
+args=""
+while [ $# -ne 0 ]; do
+  case $1 in
+    -h )
+      usage
+      exit 1
+      ;;
+    -v )
+      verbose=1
+      ;;
+    * )
+      args="$args $1"
+      ;;
+  esac
+  
+  shift
+done
+
+if [ -z "$args" ]; then
+  help
+  exit 1
+fi
+
+for arg in $args; do
+  num=`LANG=C type $arg | grep 'function' | wc -l`
+  if [ $num -ne 0 ]; then
+    $arg
+  else
+    #echo "ERROR : $arg is not shell function"
+    #exit 1
+    default $arg
+  fi
+done
+
