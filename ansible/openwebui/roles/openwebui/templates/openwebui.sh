174b15486728bdeb6576a69c3b444297d98a2b98
Kojiro ONO <ono.kojiro@gmail.com>
2025-05-21 22:10:08 +0900

    remove pyenv

--- ansible/openwebui/roles/openwebui/templates/openwebui.sh
+++ ansible/openwebui/roles/openwebui/templates/openwebui.sh
@@ -5,12 +5,13 @@
 # WEBUI_URL
 # PORT: (can not set!)
 
-. /var/lib/openwebui/pyenv/pyenv.shrc
+. /etc/openwebui/openwebui.conf
+
 cd /var/lib/openwebui
 
-DATA_DIR=/var/lib/openwebui
-WEBUI_URL=http://192.168.0.98:8080
+export DATA_DIR=/var/lib/openwebui
+export WEBUI_URL=http://192.168.0.98:8080
 
-#exec /var/lib/openwebui/.local/bin/uvx --python 3.11 open-webui@latest serve --port 8080
-exec /var/lib/openwebui/.local/bin/uvx --python 3.11 open-webui@0.5.17 serve --port 8080
+exec /var/lib/openwebui/.local/bin/uvx --python 3.11 \
+  open-webui@latest serve --port 8080
 
