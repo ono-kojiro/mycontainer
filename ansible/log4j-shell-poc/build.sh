c82c7a81bdc785f048256151dc9c1b1fd85174a9
Kojiro ONO <ono.kojiro@gmail.com>
2025-08-03 23:26:35 +0900

    playbook for log4j-shell-poc

---
+++ ansible/log4j-shell-poc/build.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+flags=""
+
+help()
+{
+  usage
+}
+
+usage()
+{
+  cat << EOS
+usage : $0 [options] target1 target2 ...
+
+target:
+  deploy
+EOS
+
+}
+
+all()
+{
+  deploy
+}
+
+hosts()
+{
+  ansible-inventory -i template.yml --list --yaml > hosts.yml
+}
+
+deploy()
+{
+   ansible-playbook $flags -i hosts.yml site.yml
+}
+
+default()
+{
+  tag=$1
+  ansible-playbook $flags -i hosts.yml -t $tag site.yml
+}
+
+victim()
+{
+  ansible-playbook $flags -i hosts.yml victim.yml
+}
+
+attacker()
+{
+  ansible-playbook $flags -i hosts.yml attacker.yml
+}
+
+
+hosts
+
+args=""
+while [ $# -ne 0 ]; do
+  case $1 in
+    -h )
+      usage
+      exit 1
+      ;;
+    -v )
+      verbose=1
+      ;;
+	-* )
+	  flags="$flags $1"
+	  ;;
+    * )
+      args="$args $1"
+      ;;
+  esac
+  
+  shift
+done
+
+if [ -z "$args" ]; then
+  help
+  exit 1
+fi
+
+for arg in $args; do
+  num=`LANG=C type $arg | grep 'function' | wc -l`
+  if [ $num -ne 0 ]; then
+    $arg
+  else
+    #echo "ERROR : $arg is not shell function"
+    #exit 1
+    default $arg
+  fi
+done
+
