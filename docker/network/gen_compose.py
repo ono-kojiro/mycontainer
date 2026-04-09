4915f5a3019ace09ac903880e329812e395d0d23
Kojiro ONO <ono.kojiro@gmail.com>
2026-04-09 20:18:51 +0900

    add flags for macvlan/management

--- docker/network/gen_compose.py
+++ docker/network/gen_compose.py
@@ -5,6 +5,9 @@ print('services:')
 
 num_vm = 7
 
+use_macvlan = 1
+use_mgmt = 0
+
 for i in range(1, num_vm + 1) :
     print('  client{0}:'.format(i))
     print('    image: ${{CLIENT{0}_IMAGE}}'.format(i))
@@ -23,18 +26,29 @@ for i in range(1, num_vm + 1) :
     print('    ports:')
     print('      - "${{CLIENT{0}_LPORT}}:22"'.format(i))
     print('    networks:')
-    print('      lan{0}_macvlan:'.format(i))
+    if use_macvlan :
+      print('      lan{0}_macvlan:'.format(i))
+    else:
+      print('      lan{0}_bridge:'.format(i))
     print('        ipv4_address: ${{CLIENT{0}_IPV4}}'.format(i))
-    print('      mgmt_bridge:')
-    print('        ipv4_address: ${{CLIENT{0}_MGMT_IPV4}}'.format(i))
+    if use_mgmt:
+      print('      mgmt_bridge:')
+      print('        ipv4_address: ${{CLIENT{0}_MGMT_IPV4}}'.format(i))
+    
     print('')
 
 print('networks:')
 for i in range(1, num_vm + 1) :
-    print('  lan{0}_macvlan:'.format(i))
-    #print('    name: lan{0}_bridge'.format(i))
-    #print('    driver: bridge')
-    print('    driver: macvlan')
+    if use_macvlan :
+      print('  lan{0}_macvlan:'.format(i))
+    else :
+      print('  lan{0}_bridge:'.format(i))
+    print('    name: lan{0}_bridge'.format(i))
+    if use_macvlan :
+      print('    driver: macvlan')
+    else :
+      print('    driver: bridge')
+
     print('    driver_opts:')
     print('      parent: ${{CLIENT{0}_PARENT}}'.format(i))
     print('    ipam:')
@@ -43,9 +57,10 @@ for i in range(1, num_vm + 1) :
     print('          gateway: ${{CLIENT{0}_GATEWAY}}'.format(i))
     print('')
 
-print('  mgmt_bridge:')
-print('    driver: bridge')
-print('    ipam:')
-print('      config:')
-print('        - subnet: 172.31.0.0/24')
+if use_mgmt :
+  print('  mgmt_bridge:')
+  print('    driver: bridge')
+  print('    ipam:')
+  print('      config:')
+  print('        - subnet: 172.31.0.0/24')
 
