e8bbbfcc10e61f317cd89fe4ed34f38d069e5ef2
Kojiro ONO <ono.kojiro@gmail.com>
2026-05-02 13:42:53 +0900

    add playbook for grafana

---
+++ ansible/grafana/build.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+flags=""
+
+help()
+{
+  usage
+}
+
+usage()
+{
+  cat << EOS
+usage : $0 [options] target1 target2 ...
+
+  target:
+    deply
+    reset
+EOS
+
+}
+
+all()
+{
+  deploy
+}
+
+clean()
+{
+  ansible-playbook -i hosts.yml clean.yml
+}
+
+hosts()
+{
+  ansible-inventory -i inventory.yml --list --yaml > hosts.yml
+}
+
+install()
+{
+  ansible-playbook $flags -i hosts.yml -t install site.yml
+}
+
+deploy()
+{
+  ansible-playbook $flags -i hosts.yml site.yml
+}
+
+default()
+{
+  tag=$1
+  ansible-playbook $flags -i hosts.yml -t ${tag} site.yml
+}
+
+destroy()
+{
+  ansible-playbook $flags -i hosts.yml destroy.yml
+}
+
+hosts
+
+args=""
+while [ "$#" -ne 0 ]; do
+  case $1 in
+    -h )
+      usage
+      exit 1
+      ;;
+    -v )
+      verbose=1
+      ;;
+    -* )
+      flags="$flags $1"
+      ;;
+    * )
+      args="$args $1"
+      ;;
+  esac
+  
+  shift
+done
+
+if [ -z "$args" ]; then
+  help
+  exit 1
+fi
+
+for target in $args; do
+  target=`echo $target | tr '-' '_'`
+  num=`LANG=C type $target 2>&1 | grep 'function' | wc -l`
+  if [ "$num" -ne 0 ]; then
+    $target
+  else
+    #echo "ERROR : $target is not shell function"
+    #exit 1
+    default $target
+  fi
+done
+
