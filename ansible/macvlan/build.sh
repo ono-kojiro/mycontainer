c7e3391a0d2be3d67fbccc150525fdff43a24f9c
Kojiro ONO <ono.kojiro@gmail.com>
2026-07-25 17:17:40 +0900

    add playbook for macvlan

---
+++ ansible/macvlan/build.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+cd $top_dir
+
+flags=""
+
+help()
+{
+  usage
+}
+
+usage()
+{
+  cat << EOS
+usage : $0 [options] target1 target2 ...
+
+  target:
+    deploy
+EOS
+
+}
+
+all()
+{
+  deploy
+}
+
+hosts()
+{
+  ansible-inventory -i inventory.yml --list --yaml > hosts.yml
+}
+
+deploy()
+{
+  ansible-playbook $flags -i hosts.yml site.yml
+}
+
+default()
+{
+  tag=$1
+  ansible-playbook $flags -i hosts.yml -t $tag site.yml
+}
+
+hosts
+
+args=""
+while [ "$#" -ne 0 ]; do
+  case $1 in
+    -h )
+      usage
+      exit 1
+      ;;
+    -v )
+      verbose=1
+      ;;
+    -* )
+      flags="$flags $1"
+      ;;
+    * )
+      args="$args $1"
+      ;;
+  esac
+  
+  shift
+done
+
+if [ -z "$args" ]; then
+  help
+  exit 1
+fi
+
+for target in $args; do
+  target=`echo $target | tr '-' '_'`
+  num=`LANG=C type $target | grep 'function' | wc -l`
+  if [ $num -ne 0 ]; then
+    $target
+  else
+    default $target
+  fi
+done
+
