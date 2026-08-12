2dd964b4eb11934cd65ba95167fee8f15280aa67
Kojiro ONO <ono.kojiro@gmail.com>
2026-08-12 17:28:19 +0900

    enable tls

--- ansible/seaweedfs/build.sh
+++ ansible/seaweedfs/build.sh
@@ -47,6 +47,73 @@ default()
   ansible-playbook $flags -i hosts.yml -t ${tag} site.yml
 }
 
+test_http()
+{
+  endpoint_url="http://localhost:8333"
+  aws_opts="--endpoint-url $endpoint_url"
+  
+  run_test
+}
+
+test_https()
+{
+  . ./.env
+
+  export AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY
+  export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+
+  endpoint_url="https://localhost:8333"
+  aws_opts="--endpoint-url $endpoint_url --no-verify-ssl"
+
+  run_test
+}
+
+run_test()
+{
+  exp=`uuidgen`
+  echo "INFO: exp is $exp"
+  rm -f test.txt
+
+  aws $aws_opts s3 rb s3://mybucket --force || true
+  aws $aws_opts s3 mb s3://mybucket
+  echo -n "$exp" > test.txt
+  aws $aws_opts s3 cp test.txt s3://mybucket/test.txt
+
+  rm -f test.txt
+
+  aws $aws_opts s3 ls s3://mybucket
+  aws $aws_opts s3 cp s3://mybucket/test.txt ./test.txt
+
+  got=`cat test.txt`
+  echo "INFO: got is $got"
+  if [ "$exp" = "$got" ]; then
+    echo "TEST: passed"
+  else
+    echo "TEST: failed"
+  fi
+}
+
+orig_test_https()
+{
+  . ./.env
+
+  export AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY
+  export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+
+  endpoint_url="https://localhost:8333"
+  aws_opts="--endpoint-url $endpoint_url --no-verify-ssl"
+
+  aws $aws_opts s3 rb s3://mybucket --force || true
+  aws $aws_opts s3 mb s3://mybucket
+  date > hello.txt
+  aws $aws_opts s3 cp hello.txt s3://mybucket/hello.txt
+  aws $aws_opts s3 ls s3://mybucket
+  aws $aws_opts s3 cp s3://mybucket/hello.txt ./hello_downloaded.txt
+  cat hello_downloaded.txt
+}
+
 hosts
 
 args=""
@@ -75,14 +142,15 @@ if [ -z "$args" ]; then
   exit 1
 fi
 
-for arg in $args; do
-  num=`LANG=C type $arg 2>&1 | grep 'function' | wc -l`
+for target in $args; do
+  target=`echo $target | tr '-' '_'`
+  num=`LANG=C type $target 2>&1 | grep 'function' | wc -l`
   if [ "$num" -ne 0 ]; then
-    $arg
+    $target
   else
-    #echo "ERROR : $arg is not shell function"
+    #echo "ERROR : $target is not shell function"
     #exit 1
-    default $arg
+    default $target
   fi
 done
 
