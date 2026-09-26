6c917e06c9956f8fb12be1064889b422fa8288e5
Kojiro ONO <ono.kojiro@gmail.com>
2026-09-26 13:29:34 +0900

    split jenkins playbook and nginx playbook

--- ansible/jenkins/build.sh
+++ ansible/jenkins/build.sh
@@ -3,6 +3,8 @@
 top_dir="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 cd $top_dir
 
+flags=""
+
 help()
 {
   usage
@@ -28,18 +30,18 @@ all()
 
 hosts()
 {
-  ansible-inventory -i template.yml --list --yaml > hosts.yml
+  ansible-inventory -i inventory.yml --list --yaml > hosts.yml
 }
 
 deploy()
 {
-  ansible-playbook -K -i hosts.yml site.yml
+  ansible-playbook $flags -i hosts.yml site.yml
 }
 
 default()
 {
   tag=$1
-  ansible-playbook -K -i hosts.yml -t $tag site.yml
+  ansible-playbook $flags -i hosts.yml -t $tag site.yml
 }
 
 hosts
@@ -53,6 +55,9 @@ while [ $# -ne 0 ]; do
       ;;
     -v )
       verbose=1
+      ;;
+    -*)
+      flags="$flags $1"
       ;;
     * )
       args="$args $1"
